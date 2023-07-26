e1b52f1a7f46d7604fd07061634c82b057662960
GreshAnt <138433612+GreshAnt@users.noreply.github.com>
2023-07-26 18:21:41 +0800

    Add files via upload
    Generate a session file

---
+++ CreateFile.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+import base64
+import hashlib
+def generate_unique_encrypted_file(device_id):
+    file_path = "C:\\Users\\Ruoteng\\Desktop\\device_id.bin"
+
+    key = base64.urlsafe_b64encode(hashlib.sha256(device_id.encode()).digest())
+    cipher_suite = Fernet(key)
+    plaintext = device_id.encode()
+    ciphertext = cipher_suite.encrypt(plaintext)
+    with open(file_path, "wb") as file:
+        file.write(ciphertext)
+hwid = input('>hwid: ')
+generate_unique_encrypted_file(hwid)
